40ee6ce90899459cd656c2dd0b8fa13a1d424983
SomeAnxiousIguanaa <69869347+SomeAnxiousIguanaa@users.noreply.github.com>
2020-10-12 13:10:24 -0700

    Added Database

--- src/main/java/edu/jsu/mcis/tas_fa20/TASDatabase.java
+++ src/main/java/edu/jsu/mcis/tas_fa20/TASDatabase.java
@@ -1,14 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.jsu.mcis.tas_fa20;
 
-/**
- *
- * @author SomeAnxiousIguana
- */
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.*;
+
 public class TASDatabase {
-    
+    public TASDatabase(){
+        Connection conn = null;
+		PreparedStatement pstUpdate = null;
+        PreparedStatement pstSelect = null,	
+        ResultSet resultset = null;
+        ResultSetMetaData metadata = null;
+
+        String query;
+        boolean hasresults;
+        int resultCount, columnCount = 0;   
+        try {
+            String username = "tasuser";
+            String password = "Password123#@!";
+			String server = ("jdbc:mysql://localhost/tas");
+            System.out.println("Connecting to " + server);
+            conn = DriverManager.getConnection(server, username, password);
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			
+            if (conn.isValid(0)) {
+                System.out.println("Connected!");
+            }
+			
+        }catch (Exception z) {
+            System.err.println(z.toString());
+        } 
+    }
 }
