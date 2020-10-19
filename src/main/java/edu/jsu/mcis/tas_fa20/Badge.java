94d91dd3030893fefd9b0ac4a322dbd5cb314fbb
SomeAnxiousIguanaa <69869347+SomeAnxiousIguanaa@users.noreply.github.com>
2020-10-18 18:02:56 -0700

    Update Badge.java

--- src/main/java/edu/jsu/mcis/tas_fa20/Badge.java
+++ src/main/java/edu/jsu/mcis/tas_fa20/Badge.java
@@ -6,22 +6,18 @@ private String description;
 private String id;
     
     public Badge(String id, String description){
-        this.id = id;
         this.description = description;
+		this.id = id;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setId(String id) {
         this.id = id;
     }
-
     public void setDescription(String description) {
         this.description = description;
-    }
-    
+    }   
     public String getID(){
         return id;
     }
@@ -29,11 +25,8 @@ private String id;
     @Override
     public String toString(){
         StringBuilder badge = new StringBuilder();
-        
         badge.append("#").append(id).append(" ");
         badge.append("(").append(description).append(")");
-        
         return(badge.toString());
-    }
-    
+    }   
 }
